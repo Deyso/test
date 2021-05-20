60d1dc4829e9873174e8dcf4567cdd10c791ba5b
deyso <bauer.dezso@gmail.com>
2021-05-20 14:55:52 +0200

    some changes

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const publicVapidKey =
 
 let btn = document.querySelector(".click-btn");
 let date = new Date();
-btn.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getUTCMilliseconds()}`;
+btn.innerHTML = `Clasdfasdf me`;
 btn.addEventListener("click", () => {
   if ("serviceWorker" in navigator) {
     send().catch((err) => console.log(err));
@@ -15,21 +15,40 @@ btn.addEventListener("click", () => {
 
 async function send() {
   console.log("Registering service worker...");
-  const register = await navigator.serviceWorker.register("/worker.js");
+  const register = await navigator.serviceWorker.register("/worker.js", {
+    scope: "/",
+  });
 
   const subscription = await register.pushManager.subscribe({
     userVisibleOnly: true,
     applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
   });
+  console.log(JSON.stringify(subscription))
+  console.log(subscription, "befooore fetch");
+  let f = { hello: "valami", ki: 444 };
+  f = JSON.stringify(f);
 
-  await fetch("/subscribe", {
+  await fetch("http://localhost:5000/subscribe", {
+    mode: "no-cors",
     method: "POST",
-    body: JSON.stringify(subscription),
+    body: JSON.stringify(f),
     headers: {
       "content-type": "application/json",
     },
   });
 }
+
+fetch("http://localhost:5000/get", { mode: "no-cors" })
+  .then((data) => {
+    console.log(data, Math.random() * 1000);
+    return data;
+  })
+  .then((list) => {
+    console.log(list);
+  });
+
+// this.loading = false;
+console.log(this.plates);
 
 function urlBase64ToUint8Array(base64String) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
