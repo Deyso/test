7216b89b63cd00d1185d2628356fec4ae7526ce2
deyso <bauer.dezso@gmail.com>
2021-05-21 17:53:47 +0200

    ggg

--- worker.js
+++ worker.js
@@ -1,8 +1,7 @@
 self.addEventListener("push", (e) => {
   const data = e.data.json();
   self.registration.showNotification(data.title, {
-    body: "Bauer Dávid vagyok buznyák",
-    // icon: "https://pbs.twimg.com/profile_images/1392386133032587264/R_8kcRAL_400x400.png",
+    body: "Content what we can show",
     icon: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/117242560_1192269644458788_2099530831760913120_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=SdQ1xPehQHkAX9J-wsD&_nc_ht=scontent-vie1-1.xx&oh=296d3845792ea3d501b4630a0e64ef6b&oe=60CBF14B",
   });
 });
